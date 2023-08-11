97a49f21cdb4f3066e4a1e5ee3fdadea874172b7
samar4988 <132490342+samar4988@users.noreply.github.com>
2023-08-11 20:00:40 +0300

    a

---
+++ python-almost_a_circle/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Creating base class"""
+
+
+class Base:
+    """base class"""
+    __nb_objects = 0
+
+    def __init__(self, id = None):
+        """Init method"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+0 comments on commit c9f431f
