d8d5ddf31ff0601ecadf4022a08a5fef486629ea
samar4988 <132490342+samar4988@users.noreply.github.com>
2023-08-21 22:42:33 +0300

    Update 8-square.py

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,53 +1,61 @@
 #!/usr/bin/python3
-"""Definition for an empty class called BaseGeometry."""
-class CustomMeta(type):
-    """A custom meta class"""
-    def __dir__(cls):
-        """override dir() method for meta class"""
-        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+"""Defines a base geometry class BaseGeometry."""
 
-class BaseGeometry(metaclass = CustomMeta):
-    """BaseGeometry class with a custom metaclass"""
-    
+
+class baseGeo(type):
+    """The custom metaclass"""
+    def __dir__(self):
+        """Overrides dir() method for the metaclass"""
+        return [attribute for attribute in super().__dir__()
+                if attribute != '__init_subclass__']
+
+
+class BaseGeometry(metaclass=baseGeo):
+    """Reprsent base geometry."""
+
+    def area(self):
+        """Not yet implemented."""
+        raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
-        """Validation for a parameter as an integer.
-
-        Args:
-            name (str): The name of the parameter.
-            value (int): The parameter to validate.
-        Raises:
+        """Validate a parameter as an integer
             TypeError: If value is not an integer.
             ValueError: If value is <= 0.
         """
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
+        elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-        
-    def area(self):
-        """new area funtion"""
-        raise Exception("area() is not implemented")
+        else:
+            return value
 
-    def __dir__(cls):
+    def __dir__(self):
         """Overrides dir() method for the metaclass"""
-        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
-    
+        return [attribute for attribute in super().__dir__()
+                if attribute != '__init_subclass__']
+
+
 class Rectangle(BaseGeometry):
-    """inherited from base class"""
+    """rectangle class"""
+
     def __init__(self, width, height):
-        self.__height = super().integer_validator("height", height)
+        """initializing width and height"""
         self.__width = super().integer_validator("width", width)
-    
-    def area(self):
-        return self.__width * self.__height
-    
+        self.__height = super().integer_validator("height", height)
+
     def __str__(self):
+        """str method"""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
-    
+
+    def area(self):
+        """rectangle area"""
+        return self.__width * self.__height
+
+
 class Square(Rectangle):
-    """Square class inherited from rectangle class"""
+    """Square class"""
 
     def __init__(self, size):
-        """initialize the square size"""
+        """initializing square size"""
         self.__size = super().integer_validator("size", size)
         super().__init__(size, size)
