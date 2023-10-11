e06c7c585f376ebc21e42e80cf643fe3fd72e88f
samar4988 <132490342+samar4988@users.noreply.github.com>
2023-10-11 17:58:28 +0300

    Update 3-dictionary_of_list_of_dictionaries.py

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,80 +1,55 @@
-#!/usr/bin/python3
-"""
-Write a Python script that, using this REST API,
-for a given employee ID, returns information about
-his/her TODO list progress
-"""
-
-import json
-import requests
-import sys
+import json, requests, sys
 
 def get_employee_data(employee_id):
-    # Define the base URL for the JSONPlaceholder API
-    base_url = "https://jsonplaceholder.typicode.com"
-
-    # Construct the URLs for employee details and TODO list
-    employee_url = f"{base_url}/users/{employee_id}"
-    todo_url = f"{base_url}/users/{employee_id}/todos"
-
-    # Fetch employee details
+    # URL to fetch employee details for all employees
+    employee_url = f"https://jsonplaceholder.typicode.com/users"
+    
     try:
-        response = requests.get(employee_url)
-        response.raise_for_status()
-        employee_data = response.json()
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching employee details: {e}")
-        sys.exit(1)
-
-    # Fetch TODO list
-    try:
-        response = requests.get(todo_url)
-        response.raise_for_status()
-        todo_data = response.json()
-    except requests.exceptions.RequestException as e:        
-        print(f"Error fetching TODO list: {e}")
-        sys.exit(1)
-
-    return employee_data, todo_data
-
-def export_todo_data(todo_data):
-    employee_todo_dict = {}
-    for task in todo_data:
-        user_id = task["userId"]
-        if user_id not in employee_todo_dict:
-            employee_todo_dict[user_id] = []
+        # Fetch data for all employees
+        response_employee = requests.get(employee_url)
+        response_employee.raise_for_status()
+        employee_data = response_employee.json()
         
-        employee_todo_dict[user_id].append({
-            "username": task["title"],
-            "task": task["title"],
-            "completed": task["completed"]
-        })
-
+        all_data = {}
+        
+        # Iterate over all employees
+        for employee in employee_data:
+            employee_id = employee["id"]
+            employee_name = employee["name"]
+            
+            # Fetch to do list
+            _, todos_data = fetch_todos(employee_id)
+            
+            # Extract completed tasks for the employee
+            completed_tasks = [{"username": employee_name, "task": todo["title"], "completed": todo["completed"]} for todo in todos_data if todo["completed"]]
+            
+            all_data[employee_id] = completed_tasks
+            
+        return all_data
+    except requests.exceptions.HTTPError as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+        
+def fetch_todos(employee_id):
+    # URL to fetch employee's to do list
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    
+    try:
+        response_todos = requests.get(todos_url)
+        response_todos.raise_for_status()
+        todos_data = response_todos.json()
+        
+        return todos_data
+    except requests.exceptions.HTTPError as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+        
+def main():
+    all_data = get_employee_data()
+    
+    # Export data to json
     with open("todo_all_employees.json", "w") as json_file:
-        json.dump(employee_todo_dict, json_file, indent=4)
-
-def display_todo_progress(employee_data, todo_data):
-    # Extract relevant information
-    employee_name = employee_data.get("name")
-    completed_tasks = [task for task in todo_data if task["completed"]]
-    total_tasks = len(todo_data)
-
-    # Display employee TODO list progress
-    print(f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{total_tasks}):")
-    for task in completed_tasks:
-        print(f"\t {task['title']}")
+        json.dump(all_data, json_file)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
-
-    try:
-        employee_id = int(sys.argv[1])
-    except ValueError:
-        print("Employee ID must be an integer.")
-        sys.exit(1)
-
-    employee_data, todo_data = get_employee_data(employee_id)
-    display_todo_progress(employee_data, todo_data)
-    export_todo_data(todo_data)
+    main()
