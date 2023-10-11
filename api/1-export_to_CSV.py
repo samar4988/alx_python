f729c6b249a2ed574d2b73998bac26d600721374
samar4988 <132490342+samar4988@users.noreply.github.com>
2023-10-11 17:57:16 +0300

    Update 1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,29 +1,20 @@
 #!/usr/bin/python3
-"""
-Using what you did in the task #0, extend your Python
-script to export data in the CSV format.
-"""
+# csv exported
 import csv
-import json
-import requests
-import sys
+from requests import get
+from sys import argv
 
-if __name__ == "__main__":
-    employee_id = sys.argv[1]
-    api_request = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(employee_id))
-    api_request = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id))
-    data = api_request.text
-    pjson = json.loads(data)
-    data1 = api_request1.text
-    pjson1 = json.loads(data1)
-    
-    #Export data to CSV data
-    filename = "{}.csv".format(employee_id)
-    with open(filename, 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, quoting = csv.QUOTE_ALL)
-        for item in pjson1:
-            user_id = employee_id
-            username = pjson['username']
-            task_completed_status = item['completed']
-            task_title = item['title']
-            writer.writerow([user_id, username, task_completed_status, task_title])
+
+def cvsWrite(user):
+    """writes to csv"""
+    data = get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+        user)).json()
+    name = get('https://jsonplaceholder.typicode.com/users/{}'.format(
+        user)).json().get('username')
+    employ_data = open('{}.csv'.format(user), 'w')
+    cwrite = csv.writer(employ_data, quoting=csv.QUOTE_ALL)
+    for line in data:
+        lined = [line.get('userId'), name,
+                 line.get('completed'), line.get('title')]
+        cwrite.writerow(lined)
+    employ_data.close()
