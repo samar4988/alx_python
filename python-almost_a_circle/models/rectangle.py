dd16e1276d0e75a5a2fa4c386c1367824c5772ec
samar4988 <132490342+samar4988@users.noreply.github.com>
2023-10-23 23:41:49 +0300

    Update rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -6,23 +6,14 @@ from models.base import Base
 """importing from Model.base"""
 
 class Rectangle(Base):
-    """
-    Represents a rectangle.
-
-    Attributes:
-        id (int): The identifier of the rectangle.
-        width (int): The width of the rectangle.
-        height (int): The height of the rectangle.
-        x (int): The x-coordinate of the rectangle's position.
-        y (int): The y-coordinate of the rectangle's position.
-    """
+     """class"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initializes a new Rectangle instance."""
-        super().__init__(id)
-        self.__width = width
-        self.__height = height
+        """ class constructor"""
+        self.width = width
+        self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
 
     @property
     def width(self):
